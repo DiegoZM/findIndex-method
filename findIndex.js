83737d3cffa0a8bb9f7c5d7783466956b0ab6378
DiegoZM <diegozm2008@gmail.com>
2022-10-04 21:54:40 -0600

    findIndex method example works

---
+++ findIndex.js
@@ -0,0 +1,6 @@
+const consolas = ["Play Station 4", "Nintendo Switch", "Xbox Series X", "Sega Genesis Mini", "SNES Classic Edition"];
+
+const index = consolas.findIndex(consola => consola === "Xbox Series X");
+
+console.log(index); 
+console.log(consolas[index]); 
